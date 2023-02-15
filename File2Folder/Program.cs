756f48318f01acc62a3a6320e76ac3a187ba6eea
salehi.m <salehi.m@SALEHI>
2023-02-15 12:11:06 +0330

    Add project files.

---
+++ File2Folder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace File2Folder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filePaths = Directory.GetFiles(@"F:\mali\Files", "*.pdf");
+
+            foreach (string filePath in filePaths)
+            {
+                string directoryPath = Path.GetDirectoryName(filePath);
+                string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(filePath);
+                string newDirectoryPath = Path.Combine(directoryPath, fileNameWithoutExtension);
+
+                if (!Directory.Exists(newDirectoryPath))
+                {
+                    Directory.CreateDirectory(newDirectoryPath);
+                }
+
+                string newFilePath = Path.Combine(newDirectoryPath, Path.GetFileName(filePath));
+                File.Move(filePath, newFilePath);
+            }
+        }
+    }
+}
